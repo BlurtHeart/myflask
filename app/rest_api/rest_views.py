e5a7ee41b749bb092878019e570849a25753697c
blurtheart <fuhongbofhb@163.com>
2017-04-24 17:09:21 +0800

    add user register to rest api

--- app/rest_api/rest_views.py
+++ app/rest_api/rest_views.py
@@ -9,11 +9,12 @@
     :license: BSD, see LICENSE for more details.
 """
 from . import rest_api
-from flask import request, Response
+from flask import request, Response, abort
 from ..models import User, Role
 import json
 from ..sqllib import DataBaseClient
 from ..libs.jsonlib import json_response
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 @rest_api.route('/users')
@@ -23,3 +24,36 @@ def rest_index():
     n = db.execute(sql)
     ret = db.fetchall()
     return json_response(ret)
+
+
+@rest_api.route('/user/register', methods=['POST'])
+def rest_register_user():
+    email = request.json.get('email', None)
+    name = request.json.get('username', None)
+    password = request.json.get('password', None)
+    confirm_password = request.json.get('confirm_password', None)
+
+    # check incoming params
+    if None in (name, email, password, confirm_password):
+        return json_response({"result":0, "reason":"params may be wrong"})
+    if password != confirm_password:
+        return json_response({"request":request.json, "result":0, "reason":"password not match"})
+    if len(password) < 6:
+        return json_response({"result":0, "reason":"password too short"})
+
+    password_hash = generate_password_hash(password)
+
+    try:
+        sql = "insert into users(name, email, passwd_hash, role_id) values('%s','%s','%s',1)" % \
+            (str(name), str(email), password_hash)
+        db = DataBaseClient()
+        n = db.execute(sql)
+        if n is 0:
+            result = 0
+        else:
+            result = 1
+        reason = ''
+    except Exception, exp:
+        result = 0
+        reason = str(exp)
+    return json_response({"result":result, "reason":reason})
