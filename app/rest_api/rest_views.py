07c8ceb2b6f3e612f026208b9aa04ba2671e6517
blurtheart <fuhongbofhb@163.com>
2017-05-09 17:51:38 +0800

    perfect rest api without some gets

--- app/rest_api/rest_views.py
+++ app/rest_api/rest_views.py
@@ -9,16 +9,18 @@
     :license: BSD, see LICENSE for more details.
 """
 from . import rest_api
-from flask import request, Response, abort, render_template, url_for
-from ..models import User, Role
+from flask import request, Response, abort, render_template, url_for, g
+from ..models import User, Role, Post, Comment
 import json
 from ..sqllib import DataBaseClient
 from ..libs.jsonlib import json_response
 from werkzeug.security import generate_password_hash, check_password_hash
+from .authentication import auth
+from datetime import datetime
 
 
 @rest_api.route('/users')
-def rest_index():
+def rest_users():
     db = DataBaseClient()
     sql = "select email from users"
     n = db.execute(sql)
@@ -27,7 +29,7 @@ def rest_index():
 
 
 @rest_api.route('/login', methods=['GET', 'POST'])
-def test():
+def rest_login():
     if request.method == 'GET':
         return render_template('jquery/login.html')
     email = request.form.get('email')
@@ -72,3 +74,125 @@ def rest_register_user():
         result = 0
         reason = str(exp)
     return json_response({"result":result, "reason":reason})
+
+
+@rest_api.route('/post', methods=['POST'])
+@auth.login_required
+def rest_post():
+    if request.json is None:
+        abort(400)
+    body = request.json.get('body', None)
+    title = request.json.get('title', None)
+    if None in (body, title):
+        return json_response({'result':0, 'reason':'params may be wrong'})
+    author_id = g.current_user.id
+    post = Post()
+    Post.on_changed_body(post, body, None, None)
+    body_html = post.body_html
+    reason = ''
+    try:
+        sql = "insert into posts(body, body_html, title, timestamp, author_id) \
+            values('%s', '%s', '%s', '%s', %d);" %(body, body_html, title, datetime.now(), author_id)
+        # db = DataBaseClient()
+        with DataBaseClient() as db:
+            n = db.execute(sql)
+            if n is 0:
+                result = 0
+            else:
+                result = 1
+    except Exception, exp:
+        result = 0
+        reason = str(exp)
+    return json_response({'result':result, 'reason':reason})
+
+
+@rest_api.route('/comment/<int:post_id>', methods=['POST'])
+@auth.login_required
+def rest_comment(post_id):
+    if request.json is None:
+        abort(400)
+    body = request.json.get('body', None)
+    if body is None or post_id == 0:
+        return json_response({'result':0, 'reason':'params may be wrong'})
+    author_id= g.current_user.id
+    comment = Comment()
+    Comment.on_changed_body(comment, body, None, None)
+    body_html = comment.body_html
+    reason = ''
+    try:
+        select_sql = "select * from posts where post_id=%d;" %post_id
+        db = DataBaseClient()
+        n = db.execute(select_sql)
+        res = db.fetchone()
+        if not res:
+            result = 0
+            reason = 'post not found'
+            return json_response({'result':result, 'reason':reason})
+        sql = "insert into comments(body, body_html, post_id, author_id, timestamp) \
+            values('%s', '%s', %d, %d, '%s');" %(body, body_html, post_id, author_id, datetime.now())
+        n = db.execute(sql)
+        if n is 0:
+            result = 0
+        else:
+            result = 1
+    except Exception, exp:
+        result = 0
+        reason = str(exp)
+    return json_response({'result':result, 'reason':reason})
+
+
+@rest_api.route('/follower/<int:user_id>', methods=['GET', 'POST'])
+@auth.login_required
+def rest_follower(user_id):
+    if request.method == 'GET':
+        sql = "select f.follower_id, u1.email as follower_email, u1.name as follower_name \
+        from follows as f left outer join users as u1 on f.follower_id=u1.id \
+        where f.followed_id=%d;" % user_id
+        with DataBaseClient() as db:
+            n = db.execute(sql)
+            res = db.fetchall()
+        return json_response({'followers':res})
+    else:
+        reason = ''
+        result = 0
+        select_sql = 'select * from users where id=%d;' %user_id
+        follower_id = g.current_user.id
+        sql = "insert into follows(follower_id, followed_id) values(%d, %d)" %(follower_id, user_id)
+        with DataBaseClient() as db:
+            n = db.execute(select_sql)
+            res = db.fetchone()
+            if not res:
+                reason = 'user not found'
+            else:
+                n = db.execute(sql)
+                result = n
+        return json_response({'result':result, 'reason':reason})
+
+
+@rest_api.route('/followed/<int:user_id>', methods=['GET', 'POST'])
+@auth.login_required
+def rest_followed(user_id):
+    if request.method == 'GET':
+        sql = "select f.followed_id, u1.email as followed_email, u1.name as followed_name \
+        from follows as f left outer join users as u1 on f.followed_id=u1.id \
+        where f.follower_id=%d;" % user_id
+        with DataBaseClient() as db:
+            n = db.execute(sql)
+            res = db.fetchall()
+        return json_response({'followeds':res})
+    else:
+        reason = ''
+        result = 0
+        select_sql = 'select * from users where id=%d;' %user_id
+        followed_id = g.current_user.id
+        sql = "insert into follows(follower_id, followed_id) values(%d, %d);" %(user_id, followed_id)
+        with DataBaseClient() as db:
+            n = db.execute(select_sql)
+            res = db.fetchone()
+            if not res:
+                reason = 'user not found'
+            else:
+                n = db.execute(sql)
+                result = n
+        return json_response({'result':result, 'reason':reason})
+
